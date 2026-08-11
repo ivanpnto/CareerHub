a212e4613a0f60af8d9e05a6c690d1ba1faa82fd
Ivan Pinto <ipintov21@gmail.com>
2026-08-11 17:55:50 +0200

    Configurar docker, recursos...

---
+++ backend/src/main/java/com/careerhub/CareerhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.careerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerhubBackendApplication.class, args);
+	}
+
+}
